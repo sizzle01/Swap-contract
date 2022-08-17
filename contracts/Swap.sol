bcabd1b893591fcf87fad53bbb63cd6e260fdcce
sizzle01 <talktoaliyu@gmail.com>
2022-08-17 19:50:47 -0400

    fixed my comments

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -3,18 +3,18 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Exchange is ERC20 {
+contract Swap is ERC20 {
 
-  address public cryptoDevTokenAddress;
+  address public hotCoin;
 
-    // Exchange is inheriting ERC20, because our exchange would keep track of Crypto Dev LP tokens
-    constructor(address _CryptoDevtoken) ERC20("CryptoDev LP Token", "CDLP") {
-        require(_CryptoDevtoken != address(0), "Token address passed is a null address");
-        cryptoDevTokenAddress = _CryptoDevtoken;
+    // Swap is inheriting ERC20, because our exchange would keep track of  tokens
+    constructor(address _ownerAddress) ERC20("CryptoDev LP Token", "CDLP") {
+        require(_ownerAddress != address(0), "Token address passed is a null address");
+        hotCoin = _ownerAddress;
     }
 
     function getReserve() public view returns (uint) {
-    return ERC20(cryptoDevTokenAddress).balanceOf(address(this));
+    return ERC20(hotCoin).balanceOf(address(this));
 }
 
 function getAmountOfTokens(
@@ -23,28 +23,22 @@ function getAmountOfTokens(
     uint256 outputReserve
 ) public pure returns (uint256) {
     require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
-    // We are charging a fee of `1%`
-    // Input amount with fee = (input amount - (1*(input amount)/100)) = ((input amount)*99)/100
+    // I am charging a fee of `1%`
+   
     uint256 inputAmountWithFee = inputAmount * 99;
-    // Because we need to follow the concept of `XY = K` curve
-    // We need to make sure (x + Δx) * (y - Δy) = x * y
-    // So the final formula is Δy = (y * Δx) / (x + Δx)
-    // Δy in our case is `tokens to be received`
-    // Δx = ((input amount)*99)/100, x = inputReserve, y = outputReserve
-    // So by putting the values in the formulae you can get the numerator and denominator
+    // following  the concept of `XY = K` curve
+   
     uint256 numerator = inputAmountWithFee * outputReserve;
     uint256 denominator = (inputReserve * 100) + inputAmountWithFee;
     return numerator / denominator;
 }
 
-function ethToCryptoDevToken(uint _minTokens) public payable {
+function ethToHotCoin(uint _minTokens) public payable {
     uint256 tokenReserve = getReserve();
-    // call the `getAmountOfTokens` to get the amount of Crypto Dev tokens
+    // invoke `getAmountOfTokens` to get the amount of Crypto Dev tokens
     // that would be returned to the user after the swap
     // Notice that the `inputReserve` we are sending is equal to  
     // `address(this).balance - msg.value` instead of just `address(this).balance`
-    // because `address(this).balance` already contains the `msg.value` user has sent in the given call
-    // so we need to subtract it to get the actual input reserve
     uint256 tokensBought = getAmountOfTokens(
         msg.value,
         address(this).balance - msg.value,
@@ -53,10 +47,10 @@ function ethToCryptoDevToken(uint _minTokens) public payable {
 
     require(tokensBought >= _minTokens, "insufficient output amount");
     // Transfer the `Crypto Dev` tokens to the user
-    ERC20(cryptoDevTokenAddress).transfer(msg.sender, tokensBought);
+    ERC20(hotCoin).transfer(msg.sender, tokensBought);
 }
 
-function cryptoDevTokenToEth(uint _tokensSold, uint _minEth) public {
+function cryptoHotCoinToEth(uint _tokensSold, uint _minEth) public {
     uint256 tokenReserve = getReserve();
     // call the `getAmountOfTokens` to get the amount of Eth
     // that would be returned to the user after the swap
@@ -67,7 +61,7 @@ function cryptoDevTokenToEth(uint _tokensSold, uint _minEth) public {
     );
     require(ethBought >= _minEth, "insufficient output amount");
     // Transfer `Crypto Dev` tokens from the user's address to the contract
-    ERC20(cryptoDevTokenAddress).transferFrom(
+    ERC20(hotCoin).transferFrom(
         msg.sender,
         address(this),
         _tokensSold
